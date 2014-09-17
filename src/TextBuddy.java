2952f8f5744a2b9642224f6101b2bfb61fe545c3
BenedictSu <Benedict_S_S@u.nus.edu>
2014-09-18 02:28:29 +0800

    Revert "Revert "simplify the method delete""
    This reverts commit 20649a998f78e90eb7bd628a093814520a4f77c2.

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -221,22 +221,16 @@ public class TextBuddy {
         int lineNumber = firstLineNumber;
 
         try {
-            if (nextLine != null) {
-                if (lineNumber == inputNumber) {
-                    newInput = nextLine;
-                    nextLine = inputFile.readLine();
-                } else {
-                    fileContent = nextLine;
-                    nextLine = inputFile.readLine();
-                    lineNumber++;
-                }
-            }
             while (nextLine != null) {
                 if (lineNumber == inputNumber) {
                     newInput = nextLine;
                     nextLine = inputFile.readLine();
                 } else {
-                    fileContent = fileContent + "\n" + nextLine;
+                    if (lineNumber == 1) {
+                        fileContent = nextLine;
+                    } else {
+                        fileContent = fileContent + "\n" + nextLine;
+                    }
                     nextLine = inputFile.readLine();
                     lineNumber++;
                 }
