17c692e68ddcda404f281af4c0e0b7c275c3747f
BenedictSu <Benedict_S_S@u.nus.edu>
2014-09-18 03:17:19 +0800

    TDD2
    - add test case for expected feedback message
    - remove unnecessary try and catch blocks

--- src/TextBuddyTest.java
+++ src/TextBuddyTest.java
@@ -9,40 +9,27 @@ public class TextBuddyTest {
 
     @Test
     public void testAdd() throws IOException{
-        try {
             assertEquals("added to test.txt: \"little brown fox\"", TextBuddy.add("add little brown fox"));
-        } catch (IOException e) {
-            throw e;
-        }
     }
     
     @Test
     public void testClear() throws IOException{
-        try {
             assertEquals("all content deleted from test.txt", TextBuddy.clear());
-        } catch (IOException e) {
-            throw e;
-        }
     }
     
     @Test
     public void testDisplay() throws IOException{
-        try {
             TextBuddy.add("add little brown fox");
             assertEquals("1. little brown fox", TextBuddy.display());
             TextBuddy.clear();
             assertEquals("test.txt is empty", TextBuddy.display());
             TextBuddy.add("add little brown fox");
             TextBuddy.add("add jump over the moon");
-            assertEquals("1. little brown fox\n2. jump over the moon", TextBuddy.display());          
-        } catch (IOException e) {
-            throw e;
-        }
+            assertEquals("1. little brown fox\n2. jump over the moon", TextBuddy.display());
     }
     
     @Test
     public void testDelete() throws IOException{
-        try {
             TextBuddy.clear();
             TextBuddy.add("add little brown fox");
             assertEquals("deleted from test.txt: \"little brown fox\"", TextBuddy.delete("delete 1"));
@@ -57,13 +44,11 @@ public class TextBuddyTest {
             assertEquals("deleted from test.txt: \"little brown fox\"", TextBuddy.delete("delete 1"));
             assertEquals("deleted from test.txt: \"jump over the moon\"", TextBuddy.delete("delete 1"));
             assertEquals("test.txt is empty", TextBuddy.display());
-        } catch (IOException e) {
-            throw e;
-        }
     }
     
-    public void testSort() {
-        assertEquals(null, TextBuddy.sort());
+    public void testSort() throws IOException {
+        TextBuddy.add("add little brown fox");
+        assertEquals("test.txt is sorted", TextBuddy.sort());
     }
 
 }
